91cfa7ca9c2c0b4f967ea114a80891970d3e2419
Gustavo Cesar <gustavo@webadapte.com>
2021-02-24 12:05:59 -0300

    Ajustes no countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
+let timeoutCountdown : NodeJS.Timeout;
+
 export function Countdown(){
-    const [time, setTime] = useState(25 * 60);
+    const [time, setTime] = useState(0.1 * 60);
     const [active, setActive] = useState(false);
+    const [hasFineshed, setHasFineshed] = useState(false);
 
     const minutes = Math.floor( time/60 );
     const seconds = time % 60;
@@ -12,14 +15,24 @@ export function Countdown(){
     const [secondLeft,secondRight] = String(seconds).padStart(2,'0').split('');
 
     function startCountdown(){
-        setActive(!active);
+        setActive(true);
+    }
+
+    function resetCountdown(){
+        clearTimeout(timeoutCountdown);
+        setActive(false);
+        setTime(0.1*60);
     }
 
     useEffect(()=>{
         if (active && time > 0){
-            setTimeout(()=>{
+            timeoutCountdown = setTimeout(()=>{
                 setTime(time - 1);
             },1000)
+        } else if(time == 0){
+            console.log("Finalizou")
+            setHasFineshed(true);
+            setActive(false);
         }
     }, [active, time])
 
@@ -36,20 +49,33 @@ export function Countdown(){
                     <span>{secondRight}</span>
                 </div>
             </div>
-            <button onClick={startCountdown} type="button" className={active ? styles.countdownPauseButton : styles.countdownButton} >
-                {
-                    active ? 
-                    <div>                        
-                        <p>Abandonar ciclo</p> 
-                        <img src="icons/close.svg" alt="Close"/>
-                    </div>
-                    : 
-                    <div>
-                        <p>Iniciar timer</p>
-                        <img src="icons/play_arrow.svg" alt="Play"/>
-                    </div>
-                }
-            </button>
+            {
+                hasFineshed ? (
+                    <button 
+                        disabled 
+                        className={styles.countdownButton} >
+                        <div>
+                            <p>Ciclo encerrado</p>
+                        </div>
+                    </button>
+                ) : (
+                    <button onClick={active ? resetCountdown : startCountdown} type="button" className={active ? styles.countdownPauseButton : styles.countdownButton} >
+                        {
+                            active ? 
+                            <div>                        
+                                <p>Abandonar ciclo</p> 
+                                <img src="icons/close.svg" alt="Close"/>
+                            </div>
+                            : 
+                            <div>
+                                <p>Iniciar timer</p>
+                                <img src="icons/play_arrow.svg" alt="Play"/>
+                            </div>
+                        }
+                    </button>
+                )
+            }
+            
         </div>
     );
 }
