8dd0049dbd6ea630cc8a061d5a6ead41675a24e5
Gustavo Cesar <gustavo@webadapte.com>
2021-02-25 11:10:23 -0300

    Adicionando notificação

--- src/contexts/challengesContext.tsx
+++ src/contexts/challengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import challenges from '../../challenges.json';
 
 export const ChallengesContext = createContext({} as ChallengesProviderData);
@@ -34,6 +34,10 @@ export function ChallengesProvider({ children } : ChallengesProviderProps){
 
     const experienceToNextLevel = Math.pow((level + 1) * 4 , 2)
 
+    useEffect(()=>{
+        Notification.requestPermission();
+    },[])
+
     function levelUp(){
         setLevel(level + 1);
     }
@@ -41,7 +45,17 @@ export function ChallengesProvider({ children } : ChallengesProviderProps){
     function startNewChallenge(){
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
+
         setActiveChallenge(challenge);
+
+        new Audio("/notification.mp3").play();
+
+        if(Notification.permission === 'granted'){
+            new Notification("Novo desafio",{
+                body: `Valendo ${challenge.amount} de xp`,
+            });
+            console.log("teste");
+        }
     }
 
     function resetChallenge(){
